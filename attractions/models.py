0330ad486cd2bc430ac1da3678e74b5b9b5bc0a0
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-10-18 00:53:01 -0300

    Evol

--- attractions/models.py
+++ attractions/models.py
@@ -6,7 +6,7 @@ class Attraction(models.Model):
     description = models.TextField()
     business_hours = models.TextField()
     min_age = models.IntegerField()
-    photo = models.ImageField(upload_to='tourist_spots', null=True, blank=True)
+    photo = models.ImageField(upload_to='attractions', null=True, blank=True)
 
     def __str__(self):
         return self.name
