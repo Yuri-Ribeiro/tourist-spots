8c21f9a3e77f002c44145bbfbe71b364e13d0712
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-10-18 00:04:41 -0300

    Testando actions personalizadas

--- tourist_spots_core/api/viewsets.py
+++ tourist_spots_core/api/viewsets.py
@@ -1,4 +1,5 @@
 # from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from tourist_spots_core.models import TouristSpot
 from .serializers import TouristSpotSerializer
@@ -26,3 +27,11 @@ class TouristSpotViewSet(ModelViewSet):
 
     # def partial_update(self, request, *args, **kwargs):
     #     pass
+
+    @action(methods=['get'], detail=True)
+    def report(self, request, pk=None):
+        pass
+
+    @action(methods=['get'], detail=False)
+    def test(self, request):
+        pass
