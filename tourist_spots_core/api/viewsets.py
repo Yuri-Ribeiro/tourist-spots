c4715385c212c593c23773b08cb680bcf2d8a6c7
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-10-18 00:29:17 -0300

    Sobrescrevendo actions, mas mantendo comportamento padrão

--- tourist_spots_core/api/viewsets.py
+++ tourist_spots_core/api/viewsets.py
@@ -1,5 +1,5 @@
 # from rest_framework.response import Response
-from rest_framework.decorators import action
+# from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from tourist_spots_core.models import TouristSpot
 from .serializers import TouristSpotSerializer
@@ -10,28 +10,29 @@ class TouristSpotViewSet(ModelViewSet):
     def get_queryset(self):
         return TouristSpot.objects.filter(approved=True)
 
-    # def list(self, request, *args, **kwargs):
-    #     return Response({'teste': 321})
+    def list(self, request, *args, **kwargs):
+        return super(TouristSpotViewSet, self).list(request, *args, **kwargs)
 
-    # def create(self, request, *args, **kwargs):
-    #     return Response({'message': f'Olá  {request.data["name"]}'})
+    def create(self, request, *args, **kwargs):
+        # return Response({'message': f'Olá  {request.data["name"]}'})
+        return super(TouristSpotViewSet, self).create(request, *args, **kwargs)
 
-    # def destroy(self, request, *args, **kwargs):
+    def destroy(self, request, *args, **kwargs):
+        return super(TouristSpotViewSet, self).destroy(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        return super(TouristSpotViewSet, self).retrieve(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        return super(TouristSpotViewSet, self).update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        return super(TouristSpotViewSet, self).partial_update(request, *args, **kwargs)
+
+    # @action(methods=['get'], detail=True)
+    # def report(self, request, pk=None):
     #     pass
-
-    # def retrieve(self, request, *args, **kwargs):
+    #
+    # @action(methods=['get'], detail=False)
+    # def test(self, request):
     #     pass
-
-    # def update(self, request, *args, **kwargs):
-    #     pass
-
-    # def partial_update(self, request, *args, **kwargs):
-    #     pass
-
-    @action(methods=['get'], detail=True)
-    def report(self, request, pk=None):
-        pass
-
-    @action(methods=['get'], detail=False)
-    def test(self, request):
-        pass
