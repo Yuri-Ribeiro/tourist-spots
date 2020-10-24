7aca67365d454db0ba7d4ffd51cbdfd9742c1d05
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-10-24 20:40:43 -0300

    Adição de serializers aninhados

--- tourist_spots_core/api/serializers.py
+++ tourist_spots_core/api/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework.serializers import ModelSerializer
 from tourist_spots_core.models import TouristSpot
+from attractions.api.serializers import AttractionSerializer
+from addresses.api.serializers import AddressSerializer
+from comments.api.serializers import CommentSerializer
+from reviews.api.serializers import ReviewSerializer
+
 
 class TouristSpotSerializer(ModelSerializer):
+    attractions = AttractionSerializer(many=True, read_only=True)
+    address = AddressSerializer()
+    comments = CommentSerializer(many=True)
+    reviews = ReviewSerializer(many=True)
+
+
     class Meta:
         model = TouristSpot
-        fields = ('id', 'name', 'description', 'approved', 'photo')
+        fields = ('id', 'name', 'description', 'approved', 'photo',
+                  'attractions', 'comments', 'reviews', 'address')
